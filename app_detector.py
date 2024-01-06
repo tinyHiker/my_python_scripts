c139b430337393979af2dee85bf869e9e2b017ae
Taha Iqbal <tiqbal10b@gmail.com>
2024-01-06 16:04:46 -0500

    Fixed GUI for app_detector

--- app_detector.py
+++ app_detector.py
@@ -1,7 +1,11 @@
 # import modules
-from tkinter import *
+import customtkinter
+from customtkinter import *
 import winapps
  
+ 
+customtkinter.set_appearance_mode('dark')
+
 # function to attach output 
 def app():
     for item in winapps.search_installed(e.get()):
@@ -10,11 +14,16 @@ def app():
         Install_date.set(item.install_date)
         publisher.set(item.publisher)
         uninstall_string.set(item.uninstall_string)
+        return
+    
+    
+    
+    
  
  
 # object of tkinter
 # and background set for grey
-master = Tk()
+master = CTk()
 master.configure(bg='light grey')
  
 # Variable Classes in tkinter
@@ -27,34 +36,28 @@ uninstall_string = StringVar()
  
 # Creating label for each information
 # name using widget Label
-Label(master, text="Enter App name : ",
-      bg="light grey").grid(row=0, sticky=W)
-Label(master, text="Name : ",
-      bg="light grey").grid(row=2, sticky=W)
-Label(master, text="Version :",
-      bg="light grey").grid(row=3, sticky=W)
-Label(master, text="Install date :",
-      bg="light grey").grid(row=4, sticky=W)
-Label(master, text="publisher :",
-      bg="light grey").grid(row=5, sticky=W)
-Label(master, text="Uninstall string :",
-      bg="light grey").grid(row=6, sticky=W)
+CTkLabel(master, text="Enter App name : ").grid(row=0, sticky=W)
+CTkLabel(master, text="Name : ").grid(row=2, sticky=W)
+CTkLabel(master, text="Version :").grid(row=3, sticky=W)
+CTkLabel(master, text="Install date :").grid(row=4, sticky=W)
+CTkLabel(master, text="publisher :").grid(row=5, sticky=W)
+CTkLabel(master, text="Uninstall string :").grid(row=6, sticky=W)
  
  
 # Creating label for class variable
 # name using widget Entry
-Label(master, text="", textvariable=name, bg="light grey").grid(row=2, column=1, sticky=W)
-Label(master, text="", textvariable=version, bg="light grey").grid(row=3, column=1, sticky=W)
-Label(master, text="", textvariable=Install_date, bg="light grey").grid(row=4, column=1, sticky=W)
-Label(master, text="", textvariable=publisher, bg="light grey").grid(row=5, column=1, sticky=W)
-Label(master, text="", textvariable=uninstall_string, bg="light grey").grid(row=6, column=1, sticky=W)
+CTkLabel(master, text="", textvariable=name).grid(row=2, column=1, sticky=W)
+CTkLabel(master, text="", textvariable=version).grid(row=3, column=1, sticky=W)
+CTkLabel(master, text="", textvariable=Install_date).grid(row=4, column=1, sticky=W)
+CTkLabel(master, text="", textvariable=publisher).grid(row=5, column=1, sticky=W)
+CTkLabel(master, text="", textvariable=uninstall_string).grid(row=6, column=1, sticky=W)
+
  
- 
-e = Entry(master, width=30)
+e = CTkEntry(master, width = 350, height = 40)
 e.grid(row=0, column=1)
  
 # creating a button using the widget
-b = Button(master, text="Show", command=app, fg_color=("red", "red"), text_color=("white", "white"), hover_color=("red", "red"))
-b.grid(row=0, column=2, columnspan=2, rowspan=2, padx=5, pady=5)
+b = CTkButton(master, text="Show", command=app, fg_color=("red", "red"), text_color=("white", "white"), hover_color=("red", "red"))
+b.grid(row=0, column=2, columnspan=2, rowspan=2, padx=5, pady=5,)
  
 master.mainloop()
